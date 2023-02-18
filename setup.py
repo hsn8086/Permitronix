454b0714853a83d3433f069e508a3f1a7f951db3
hsn <hsn1919810@gmail.com>
2023-02-18 21:44:13 +0800

    add:setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Permitronix',
+    version='1.0.0',
+    packages=['Permitronix'],
+    url='https://github.com/hsn8086/Permitronix',
+    license='MIT',
+    author='hsn',
+    author_email='hsn1919810@gmail.com',
+    description='A py permission management module'
+)
