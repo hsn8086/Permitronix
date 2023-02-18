0cb6cb8ef6e02fa25064a264fecde3a99e2ca629
hsn <hsn1919810@gmail.com>
2023-02-19 00:22:56 +0800

    cange:ver

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='permitronix',
-    version='1.0.0',
+    name='Permitronix',
+    version='1.0.1',
     packages=['permitronix'],
     url='https://github.com/hsn8086/Permitronix',
     license='MIT',
     author='hsn',
     author_email='hsn1919810@gmail.com',
-    description='A py permission management module'
+    description='A py permission management module.'
 )
